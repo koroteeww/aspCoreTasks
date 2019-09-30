7cebdaee049367ad236e3768e8a7951cb1c0dd93
wildboar47 <wildboar47@yandex.ru>
2019-09-30 18:43:10 +0300

    30/09/2019

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -19,33 +19,70 @@ namespace aspCoreEmpty.Controllers
         }
 
         // GET: Tasks
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            return View(await _context.Tasks.ToListAsync());
+            var ListTasks = _context.Tasks.ToList();
+
+            var NewList = new List<TasksExtended>();
+            foreach (var task in ListTasks)
+            {
+                var nitem = new TasksExtended(task);
+                var NameType = _context.Types.Where(tp => tp.Idtypes == task.Idtype).FirstOrDefault().Name;
+                
+                var NameStatus = _context.Statuses.Where(stat => stat.Idstatuses == task.Idstatus).FirstOrDefault().Name;
+
+                nitem.NameType = NameType;
+                nitem.NameStatus = NameStatus;
+                NewList.Add(nitem);
+            }
+            return View(NewList);
+            
         }
 
         // GET: Tasks/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public  IActionResult Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var tasks = await _context.Tasks
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var tasks = _context.Tasks.FirstOrDefault(m => m.Id == id);
             if (tasks == null)
             {
                 return NotFound();
             }
+            var nitem = new TasksExtended(tasks);
+            var NameType = _context.Types.Where(tp => tp.Idtypes == tasks.Idtype).FirstOrDefault().Name;
 
-            return View(tasks);
+            var NameStatus = _context.Statuses.Where(stat => stat.Idstatuses == tasks.Idstatus).FirstOrDefault().Name;
+
+            nitem.NameType = NameType;
+            nitem.NameStatus = NameStatus;
+            return View(nitem);
         }
 
         // GET: Tasks/Create
         public IActionResult Create()
         {
-            return View();
+            var taskExt = new TasksExtended();
+            //заполняем выпадающий список для создания
+            var allTypes = _context.Types.ToList();
+            foreach (var item in allTypes)
+            {
+                //добавляем типы в список
+
+                taskExt.NameTypesAll.Add(item.Name);
+            }
+            //заполняем выпадающий список для создания
+            var allStatuses = _context.Statuses.ToList();
+            foreach (var item in allStatuses)
+            {
+                //добавляем статусы в список
+
+                taskExt.NameStatusesAll.Add(item.Name);
+            }
+            return View(taskExt);
         }
 
         // POST: Tasks/Create
@@ -53,14 +90,46 @@ namespace aspCoreEmpty.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Tasks tasks)
+        public async Task<IActionResult> Create(TasksExtended tasks)
         {
-            if (ModelState.IsValid)
+            //проверим ИД 
+            if (tasks.Id == 0)
             {
-                _context.Add(tasks);
+                //generate id
+                int maxid = 0;
+
+                foreach (var cat in _context.Tasks)
+                {
+                    if (cat.Id > maxid)
+                    {
+                        maxid = cat.Id;
+                    }
+                }
+                tasks.Id = maxid + 1;
+            }
+            var taskOrd = new Tasks(tasks);
+
+            //получаем имя выбранной  из модели TypesExtended
+            var SelectedTypeName = tasks.NameTypesAll[0];
+            //получаем саму категорию из базы по имени
+            var Type = _context.Types.Where(c => c.Name == SelectedTypeName).FirstOrDefault();
+            //получаем имя выбранной  из модели TypesExtended
+            var SelectedSatatusName = tasks.NameStatusesAll[0];
+            //получаем саму категорию из базы по имени
+            var Status = _context.Statuses.Where(c => c.Name == SelectedSatatusName).FirstOrDefault();
+
+            if (Type != null && Status!=null)
+            {
+                //сохраняем ИД 
+                taskOrd.Idstatus = Status.Idstatuses;
+                taskOrd.Idtype = Type.Idtypes;
+
+                //Обновляем БД
+                _context.Add(taskOrd);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(tasks);
         }
 
@@ -77,7 +146,34 @@ namespace aspCoreEmpty.Controllers
             {
                 return NotFound();
             }
-            return View(tasks);
+
+            //получаем имя type которая задана в типе по ИД
+            var NameStatus= _context.Statuses.Where(cat => cat.Idstatuses == tasks.Idstatus).First().Name;
+            var NameType = _context.Types.Where(cat => cat.Idtypes == tasks.Idtype).First().Name;
+
+            var tasksExt = new TasksExtended(tasks);
+
+            //добавляем имя type в выпадающий список
+            tasksExt.NameTypesAll.Add(NameType);
+            var allCategories = _context.Types.ToList();
+            foreach (var item in allCategories)
+            {
+                //добавляем остальные категории в список
+                if (item.Idtypes != tasks.Idtype)
+                    tasksExt.NameTypesAll.Add(item.Name);
+            }
+            tasksExt.NameType = NameType;
+            //добавляем имя status в выпадающий список
+            tasksExt.NameStatusesAll.Add(NameStatus);
+            var allStats = _context.Statuses.ToList();
+            foreach (var item in allStats)
+            {
+                //добавляем остальные категории в список
+                if (item.Idstatuses != tasks.Idstatus)
+                    tasksExt.NameStatusesAll.Add(item.Name);
+            }
+            tasksExt.NameStatus = NameStatus;
+            return View(tasksExt);
         }
 
         // POST: Tasks/Edit/5
@@ -85,7 +181,7 @@ namespace aspCoreEmpty.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Tasks tasks)
+        public async Task<IActionResult> Edit(int id, TasksExtended tasks)
         {
             if (id != tasks.Id)
             {
@@ -96,8 +192,25 @@ namespace aspCoreEmpty.Controllers
             {
                 try
                 {
-                    _context.Update(tasks);
-                    await _context.SaveChangesAsync();
+                    var typeOrd = new Tasks(tasks);
+                    //получаем имя выбранной type из модели TasksExtended
+                    var SelectedTypeName = tasks.NameTypesAll[0];
+                    //получаем имя выбранной status из модели TasksExtended
+                    var SelectedStatusName = tasks.NameStatusesAll[0];
+                    //получаем саму type из базы по имени
+                    var Type = _context.Types.Where(c => c.Name == SelectedTypeName).FirstOrDefault();
+                    //получаем саму status из базы по имени
+                    var Status = _context.Statuses.Where(c => c.Name == SelectedStatusName).FirstOrDefault();
+                    if (Type != null && Status != null)
+                    {
+                        //сохраняем ИД 
+                        typeOrd.Idstatus = Status.Idstatuses;
+                        typeOrd.Idtype = Type.Idtypes;
+                        //Обновляем БД
+                        _context.Update(typeOrd);
+                        await _context.SaveChangesAsync();
+                    }
+                    
                 }
                 catch (DbUpdateConcurrencyException)
                 {
--- Models/Tasks.cs
+++ Models/Tasks.cs
@@ -5,6 +5,25 @@ namespace aspCoreEmpty.Models
 {
     public partial class Tasks
     {
+
+        public Tasks()
+        { }
+        public Tasks(TasksExtended basic)
+        {
+            this.Idtype = basic.Idtype;
+            this.Idstatus = basic.Idstatus;
+            this.Ispolnitel = basic.Ispolnitel;
+            this.Nomer = basic.Nomer;
+            this.Pomeschenie = basic.Pomeschenie;
+            this.Sozdatel = basic.Sozdatel;
+            this.Sozdatelemail = basic.Sozdatelemail;
+            this.Datasozdaniya = basic.Datasozdaniya;
+            this.Datazakritiya = basic.Datazakritiya;
+            this.Chtosdelat = basic.Chtosdelat;
+            this.Id = basic.Id;
+            this.Foto = basic.Foto;
+        }
+
         public int Id { get; set; }
         public int? Nomer { get; set; }
         public DateTime? Datasozdaniya { get; set; }
@@ -17,5 +36,50 @@ namespace aspCoreEmpty.Models
         public string Sozdatelemail { get; set; }
         public string Chtosdelat { get; set; }
         public byte[] Foto { get; set; }
+    }
+
+    public class TasksExtended
+    {
+        public int Id { get; set; }
+        public int? Nomer { get; set; }
+        public DateTime? Datasozdaniya { get; set; }
+        public DateTime? Datazakritiya { get; set; }
+        public string Pomeschenie { get; set; }
+        public int? Idtype { get; set; }
+        public int? Idstatus { get; set; }
+        public string Ispolnitel { get; set; }
+        public string Sozdatel { get; set; }
+        public string Sozdatelemail { get; set; }
+        public string Chtosdelat { get; set; }
+        public byte[] Foto { get; set; }
+
+        public List<string> NameTypesAll { get; set; }
+        public List<string> NameStatusesAll { get; set; }
+
+        public string NameType { get; set; }
+        public string NameStatus { get; set; }
+        public TasksExtended()
+        {
+            NameTypesAll = new List<string>();
+            NameStatusesAll = new List<string>();
+        }
+        public TasksExtended(Tasks basic)
+        {
+            this.Idtype = basic.Idtype;
+            this.Idstatus = basic.Idstatus;
+            this.Ispolnitel = basic.Ispolnitel;
+            this.Nomer = basic.Nomer;
+            this.Pomeschenie = basic.Pomeschenie;
+            this.Sozdatel = basic.Sozdatel;
+            this.Sozdatelemail = basic.Sozdatelemail;
+            this.Datasozdaniya = basic.Datasozdaniya;
+            this.Datazakritiya = basic.Datazakritiya;
+            this.Chtosdelat = basic.Chtosdelat;
+            this.Id = basic.Id;
+            this.Foto = basic.Foto;
+            
+            NameTypesAll = new List<string>();
+            NameStatusesAll = new List<string>();
+        }
     }
 }
